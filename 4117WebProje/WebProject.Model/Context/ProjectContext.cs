589ba6b2c4ea6f9c917f49d8e994736afeeeba81
Onur Şişoğlu <o.sisoglu@gmail.com>
2019-03-18 14:11:56 +0300

    Veritabanını Bitirdik. Devamı Yarın

---
+++ 4117WebProje/WebProject.Model/Context/ProjectContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebProject.Model.Entities;
+using WebProject.Model.Maps;
+
+namespace WebProject.Model.Context
+{
+    /* Bu class veri tabanı ile iletişim kurduğumuz yerdir. Code firstte hangi class(lar)ın veritabanında tablo olacağını belirttiğimiz yerdir.
+  Bu iletişimi sağlamak için EntityFramework kütüphanesi ile ilgili katmanda c#'ın sunduğu DbContext classını miras alarak yapmalıyız.*/
+
+   public class ProjectContext:DbContext
+    {
+        public ProjectContext()
+        {
+           /* Database.Connection.ConnectionString = "Server=.;Database=4117DB;uid=sa;pwd=123"; */// sql server authentication ile bağlanıyorsak
+            Database.Connection.ConnectionString = "Server=.;Database=4117DB;Trusted_Connection=true"; // windowsserver authentication ile bağlanıyorsak 
+        }
+
+
+        public DbSet<AppUser> Users { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<SubCategory> SubCategories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
+
+        //Yaptığımız map işlemlerinin Sql'e gönderilmesi dbcontext classının OnModelCreating metodunu ezerek gerçekleşebilir.
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // 1. yol (tek tek ekleyebilir Map sınıflarımız)
+            //modelBuilder.Configurations.Add(new AppUserMap());
+            //modelBuilder.Configurations.Add(new CategoryMap());
+            //modelBuilder.Configurations.Add(new SubCategoryMap());
+            //modelBuilder.Configurations.Add(new ProductMap());
+            //modelBuilder.Configurations.Add(new OrderMap());
+            //modelBuilder.Configurations.Add(new OrderDetailMap());
+
+            //2.yol (projede IMappingMarker neredeyse bulunduğu dosya mantığını  bir kütüphane olarak algılayıp direk yükleme olayı.) 
+            modelBuilder.Configurations.AddFromAssembly(typeof(IMappingMarker).Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
---
+++ 4117WebProje/WebProject.Model/Maps/AppUserMap.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebProject.Core.Map;
+using WebProject.Model.Entities;
+
+namespace WebProject.Model.Maps
+{
+   public class AppUserMap:CoreMap<AppUser>
+    {
+        public AppUserMap()
+        {
+            ToTable("Users");
+
+            Property(x => x.Name).HasMaxLength(40).IsRequired();
+            Property(x => x.LastName ).HasMaxLength(40).IsRequired();
+            Property(x => x.UserName).HasMaxLength(40).IsRequired();
+            Property(x => x.Password).HasMaxLength(20).IsRequired();
+            Property(x => x.Email).HasMaxLength(100).IsRequired();
+           
+        }
+    }
+}
